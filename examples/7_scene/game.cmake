a0ca2ef981291844b22cb45c070c8906f5263f1b
Rob Jinman <jinmanr@gmail.com>
2025-12-26 19:50:50 +0000

    Add new example

---
+++ examples/7_scene/game.cmake
@@ -0,0 +1,9 @@
+# Edit accordingly
+
+set(GAME_BUNDLE_IDENTIFIER "com.freeholdapps.scene")
+set(GAME_DISPLAY_NAME "Lithic3D Demo")
+set(GAME_SHORT_NAME "scene")
+set(GAME_VERSION_MAJOR "1")
+set(GAME_VERSION_MINOR "0")
+set(GAME_COPYRIGHT_STRING "(c) 2025 Freehold Apps Ltd")
+set(GAME_APPLE_TEAM_ID "XXXXXXXXXX")
