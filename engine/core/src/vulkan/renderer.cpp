8758e5926590d9c182e94e2f441ad35da755a212
Rob Jinman <jinmanr@gmail.com>
2026-06-07 21:29:19 +0100

    Fix shadows not rendering on osx/ios/android

--- engine/core/src/vulkan/renderer.cpp
+++ engine/core/src/vulkan/renderer.cpp
@@ -1388,7 +1388,7 @@ void RendererImpl::createShadowRenderPass()
     .format = depthFormat,
     .samples = VK_SAMPLE_COUNT_1_BIT,
     .loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR,
-    .storeOp = VK_ATTACHMENT_STORE_OP_DONT_CARE,
+    .storeOp = VK_ATTACHMENT_STORE_OP_STORE,
     .stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE,
     .stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE,
     .initialLayout = VK_IMAGE_LAYOUT_UNDEFINED,
